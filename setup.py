63b3e9bc99a38985cea24ce54c5c0da811d5db4f
Mohammed Javeed N <42910069+njaveed@users.noreply.github.com>
2019-09-14 13:47:54 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='datastore',
+      version='0.1',
+      description='File based datastore',
+      url='https://github.com/njaveed/Datastore.git',
+      author='Javeed',
+      author_email='meetjaveed11@gmail.com',
+      license='MIT',
+      packages=['datastore'],
+      zip_safe=False)
