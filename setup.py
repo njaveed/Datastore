bfc48c53d1b69d74818b98fc0061452293b4624a
Mohammed Javeed N <42910069+njaveed@users.noreply.github.com>
2019-09-14 16:11:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
 
-setup(name='datastore',
-      version='0.1',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='python-file-datastore',
+      version='1.0',
       description='File based datastore',
       url='https://github.com/njaveed/Datastore.git',
       author='Javeed',
       author_email='meetjaveed11@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       license='MIT',
-      packages=['datastore'],
+      packages=['python-file-datastore'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+		],
+		python_requires='>=3.6',
       zip_safe=False)
